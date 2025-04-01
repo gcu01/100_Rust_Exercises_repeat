860c8cfc1acdf922b25f8b780a31dd6c3b1e9d3f
Gabriel <gabrielcampeanu@gmail.com>
2025-04-01 17:21:45 +0200

    implementing the Power trait

--- project/src/exercises/power.rs
+++ project/src/exercises/power.rs
@@ -1,4 +1,7 @@
+use std::ops::Add;
+
 //pub mod private {
+
 pub trait Power {
     type Output;
     fn power(&self, n: u32) -> Self::Output;
@@ -30,7 +33,48 @@ impl Power for &u32 {
     }
 }
 
+// TODO: implement the necessary traits to make the test compile and pass.
+//  You *can't* modify the test.
+#[derive(Copy, Clone)]
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct WrappingU32 {
+    value: u32,
+}
 
+impl WrappingU32 {
+    pub fn new(value: u32) -> Self {
+        Self { value }
+    }
+}
+/* 
+pub trait Add<Rhs = Self> {
+    type Output;
+
+    // Required method
+    fn add(self, rhs: Rhs) -> Self::Output;
+} */
+
+impl Add for WrappingU32 {
+    type Output=WrappingU32;
+    fn add(self, other:Self) -> Self {
+        WrappingU32 { value: self.value + other.value }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_ops() {
+        let x = WrappingU32::new(42);
+        let y = WrappingU32::new(31);
+        let z = WrappingU32::new(u32::MAX);
+        assert_eq!(x + y + y, WrappingU32::new(104));
+    }
+}
+ 
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -47,4 +91,5 @@ mod tests{
         assert_eq!(7, *&2_u32.power(3));
     }
 }
+    
 //}
